d59440482397374c90e8bdfe976370ae3dc321bd
Asfia Hossoin <120407425+Asfia99@users.noreply.github.com>
2022-12-12 17:36:29 +0000

    Update streamlit_app.py
    menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents\' healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega & Blueberry Oatmeal')
+streamlit.text('Kale Spinach Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range egg')
