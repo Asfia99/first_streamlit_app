a14879b5c06cf471ec7d60804c9933748a6df208
Asfia Hossoin <120407425+Asfia99@users.noreply.github.com>
2022-12-13 10:26:52 +0000

    Update streamlit_app.py
    adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('My parents\' healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega & Blueberry Oatmeal')
-streamlit.text('Kale Spinach Rocket Smoothie')
-streamlit.text('Hard Boiled Free Range egg')
+streamlit.header('Breakfast Favourities')
+streamlit.text('🥣 Omega & Blueberry Oatmeal')
+streamlit.text('🥗 Kale Spinach Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free Range egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
